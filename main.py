96b13cac4755d8d678a6b4973553fbcffb32207e
hlwsid <45997695+hlwsid@users.noreply.github.com>
2025-07-09 16:32:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
+import os
 import requests
 import json
 import time
-import random 
+import random
 
-# Register the azure app first and make sure the app has the following permissions:
-# files: Files.Read.All、Files.ReadWrite.All、Sites.Read.All、Sites.ReadWrite.All
-# user: User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
-# mail: Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
-# After registration, you must click on behalf of xxx to grant administrator consent, otherwise outlook api cannot be called
+# Load credentials from environment variables
+refresh_token = os.getenv("REFRESH_TOKEN")
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
-
-
-
-
+# Register the Azure app first and make sure the app has the following permissions:
+# files: Files.Read.All, Files.ReadWrite.All, Sites.Read.All, Sites.ReadWrite.All
+# user: User.Read.All, User.ReadWrite.All, Directory.Read.All, Directory.ReadWrite.All
+# mail: Mail.Read, Mail.ReadWrite, MailboxSettings.Read, MailboxSettings.ReadWrite
+# After registration, you must click "Grant admin consent" for the permissions.
 
 calls = [
     'https://graph.microsoft.com/v1.0/me/drive/root',
     'https://graph.microsoft.com/v1.0/me/drive',
     'https://graph.microsoft.com/v1.0/drive/root',
     'https://graph.microsoft.com/v1.0/users',
-    'https://graph.microsoft.com/v1.0/me/messages',
+   .com/v1.0/me/messages',
     'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
     'https://graph.microsoft.com/v1.0/me/drive/root/children',
     'https://api.powerbi.com/v1.0/myorg/apps',
@@ -35,7 +36,6 @@ calls = [
     'https://graph.microsoft.com/v1.0/sites/root/drives'
 ]
 
-
 def get_access_token(refresh_token, client_id, client_secret):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -47,15 +47,14 @@ def get_access_token(refresh_token, client_id, client_secret):
         'client_secret': client_secret,
         'redirect_uri': 'http://localhost:53682/'
     }
-    html = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
-    jsontxt = json.loads(html.text)
-    refresh_token = jsontxt['refresh_token']
+    response = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
+    jsontxt = json.loads(response.text)
     access_token = jsontxt['access_token']
     return access_token
 
 def main():
     random.shuffle(calls)
-    endpoints = calls[random.randint(0,10)::]
+    endpoints = calls[random.randint(0, 10)::]
     access_token = get_access_token(refresh_token, client_id, client_secret)
     session = requests.Session()
     session.headers.update({
